77ec705386d64595d8dbde2beb96210432439114
Sergio <asergiop21@gmail.com>
2018-09-27 12:11:34 -0300

    disable api_client

--- app/models/api_client.rb
+++ app/models/api_client.rb
@@ -1,13 +1,13 @@
-require 'faraday'
-require 'oj'
+# require 'faraday'
+# require 'oj'
 
-client = Faraday.new(url: 'http://localhost:3000') do |config|
-    config.adapter  Faraday.default_adapter
-      config.token_auth('34a80f7c8371a97d15b07e430b97529e')
-end
+# client = Faraday.new(url: 'http://localhost:3000') do |config|
+#     config.adapter  Faraday.default_adapter
+#       config.token_auth('34a80f7c8371a97d15b07e430b97529e')
+# end
  
-response = client.post do |req|
-    req.url '/api/v1/project_types'
-      req.headers['Content-Type'] = 'application/json'
-        req.body = '{ "project_type_params": {"name": "prueba"} }'
-end
+# response = client.post do |req|
+#     req.url '/api/v1/project_types'
+#       req.headers['Content-Type'] = 'application/json'
+#         req.body = '{ "project_type_params": {"name": "prueba"} }'
+# end
