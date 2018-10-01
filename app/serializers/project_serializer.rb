537caff652c807b6adc1f16a89e6e4401b5cc94f
Sergio <asergiop21@gmail.com>
2018-10-01 10:19:43 -0300

    change properties array

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -2,6 +2,14 @@ class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :properties, :the_geom, :project_type_id
   def the_geom
     [object.the_geom.x,
-    object.the_geom.y]
+     object.the_geom.y]
+  end
+
+  def properties
+    object.properties.map do|val, index|
+      [val, index]
+
+
+    end
   end
 end
