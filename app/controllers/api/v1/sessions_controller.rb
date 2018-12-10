67a60aa36b73bbb2fe967fd3d62844084814443b
Sergio <asergiop21@gmail.com>
2018-12-10 16:34:07 -0300

    change render json session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,14 +6,17 @@ module Api
         user_mail = User.where(email: params[:email]).first
         
         if user_mail&.valid_password?(params[:password])
-          user =[]
-          user << {'email': user_mail.email}
-          user << {'authentication_token': user_mail.authentication_token}
-
+          user = {'email': user_mail.email, 'authentication_token': user_mail.authentication_token}
+          
+          companies=[]
           user_customer = user_mail.customers.all
-          user << {'url': user_customer[0].url}
-          user << {'name_company': user_customer[0].subdomain}
-          render json: {data: user}
+
+          user_customer.map do |uc|
+
+          companies << {'url': uc.url, 'name_company': uc.subdomain}
+          end
+          
+          render json: {data: user,'companies': companies }
         else
           head(:unauthorized)
         end
