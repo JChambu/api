84ee24040bf358bbb50603fd7aaabb429ae83038
Sergio <asergiop21@gmail.com>
2019-03-25 12:16:03 -0300

    uncomment project serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,16 +1,16 @@
 class ProjectSerializer < ActiveModel::Serializer
-  # attributes :id,  :the_geom, :project_type_id
+   attributes :id,  :the_geom, :project_type_id
 
-  # def the_geom
-  #   if !object.the_geom.nil?
-  #     [object.the_geom.x,
-  #     object.the_geom.y]
-  #   end
-  # end
+  def the_geom
+    if !object.the_geom.nil?
+      [object.the_geom.x,
+      object.the_geom.y]
+    end
+  end
 
-  # def properties
-  #   object.properties.map do|val, index|
-  #     [val, index]
-  #   end
-  # end
+  def properties
+    object.properties.map do|val, index|
+      [val, index]
+    end
+  end
 end
