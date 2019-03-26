f4ea136e74dbdb25a2c37e550cbc1cf308cb39de
Sergio <asergiop21@gmail.com>
2019-03-26 08:58:42 -0300

    order sync updated

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -8,7 +8,7 @@ module Api
         @params_date = params[:date].to_datetime
         @rows = Project.where("updated_at > ?",  @params_date)
         @rows = @rows.where(project_type_id: params[:project_type_id])
-        @rows = @rows.limit(100)
+        @rows = @rows.order(:updated_at)
         render json: {data: @rows}  
       end
       
