6a36a651f2a8d6b8f7acbc4248c815c1b0bb48ee
Sergio <asergiop21@gmail.com>
2019-05-13 10:33:42 -0300

    add new endpoint check quantity

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,14 @@ class Project < ApplicationRecord
   accepts_nested_attributes_for :photos
 
 
+  def self.row_quantity project_type_id, date_last_row, time_last_row
+
+    updated_date = [date_last_row, time_last_row].join(" ")
+
+    @rows = Project.where(project_type_id: project_type_id).where('updated_at > ?', updated_date).count
+
+  end
+
   def self.show_data_new project_type_id
 
     value = Project.where(project_type_id: project_type_id).limit(2).select("st_x(the_geom) as lng, st_y(the_geom) as lat, id, properties").limit(2)
