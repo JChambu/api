370b8e0e63dc767d5aa28f434d279f37348c3e8e
Sergio <asergiop21@gmail.com>
2019-08-26 10:39:18 -0300

    add validation status when update rows in projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -151,21 +151,27 @@ class Project < ApplicationRecord
   
   def self.update_rows_project_data project_data
     result_hash = {}
-
     project_data[:projects].each do |data|
       @project = Project.where(project_type_id: data[:project_type_id] ).where(id: data[:project_id]).first
-      value_name = {}
-      data['values'].each do |v,k|
-      field = ProjectField.where(id: v.to_i).select(:key).first
-        value_name.merge!("#{field.key}": k )
+      if @project.updated_at < data[:lastUpdate]
+        value_name = {}
+        data['values'].each do |v,k|
+          field = ProjectField.where(id: v.to_i).select(:key).first
+          if !field.nil?
+            value_name.merge!("#{field.key}": k )
+          end
+        end
+          update_row = {properties: value_name}
+          if @project.status_update_at < data[:status_update_at] 
+            update_row.merge!(status_update_at: data[:status_update_at], project_status_id: data[:status_id] )
+          end
+            if @project.update_attributes(update_row)
+          localID = data[:localID]
+          result_hash.merge!({"#{@project.id}": "ok"}) 
+        end
+        return [result_hash]
       end
-      if @project.update_attributes( properties: value_name)
-        localID = data[:localID]
-        result_hash.merge!({"#{@project.id}": "ok"}) 
-      end
-      
     end
-return [result_hash]
   end
   
   def self.save_rows_project_data_childs project_data_child
