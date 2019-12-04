b7795d007cf1bdaaa23c9c06da148dbca7302624
Sergio <asergiop21@gmail.com>
2019-12-04 11:30:36 -0300

    Se agregaron 2 atributos en el retorno de los datos del endpoint #11

--- app/models/project_data_child.rb
+++ app/models/project_data_child.rb
@@ -3,7 +3,7 @@ class ProjectDataChild < ApplicationRecord
 
   def self.show_data_new project_type_id, date_last_row, time_last_row, page
     updated_date = [date_last_row, time_last_row].join(" ")
-    value = Project.joins(:project_data_child).where(project_type_id: project_type_id).where('project_data_children.updated_at >= ?', updated_date).select("project_data_children.id, project_data_children.properties, project_data_children.updated_at,  project_data_children.user_id").order('project_data_children.updated_at',  'project_data_children.id').page(page).per_page(50)
+    value = Project.joins(:project_data_child).where(project_type_id: project_type_id).where('project_data_children.updated_at >= ?', updated_date).select("project_data_children.id, project_data_children.properties, project_data_children.updated_at,  project_data_children.user_id, project_data_children.project_id as project_data_id, project_data_children.project_field_id as project_field_id ").order('project_data_children.updated_at',  'project_data_children.id').page(page).per_page(50)
     data = []
     geom_text = ''
     value.each do |row|
@@ -11,7 +11,7 @@ class ProjectDataChild < ApplicationRecord
       row.properties.each do |k, v| 
         form.merge!("#{k}": v)
       end
-      data.push("id":row.id,  "form_values":form, "updated_at":row.updated_at,  "user_id": row.user_id )
+      data.push("id":row.id, "project_data_id": row.project_data_id, "project_field_id": row.project_field_id,  "form_values":form, "updated_at":row.updated_at,  "user_id": row.user_id )
       @data = data
     end
   end
