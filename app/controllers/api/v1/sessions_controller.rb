2b2d49d503b5f520cda4d39d08cf15b3bc095843
Sergio <asergiop21@gmail.com>
2019-12-05 19:43:49 -0300

    Check el estado de usuario al hacer login y al sincronizar #12

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,11 +3,10 @@ module Api
     class SessionsController < ApplicationController
 
       def create
-        user_mail = User.where(email: params[:email]).first
-        
-        if user_mail&.valid_password?(params[:password])
+        user_mail = User.where(email: params[:email]).where(active: true).first
+
+        if !user_mail.nil? || user_mail&.valid_password?(params[:password]) 
           user = {'email': user_mail.email, 'authentication_token': user_mail.authentication_token, 'user_id': user_mail.id}
-          
           companies=[]
           user_customer = UserCustomer.where(user_id: user_mail)
 
@@ -16,17 +15,13 @@ module Api
               companies << {'url': uc.customer.url, 'name_company': 'demo1', 'role_id': uc.role_id, 'id': uc.customer.id}
             else
               companies << {'url': uc.customer.url, 'name_company': uc.customer.subdomain, 'role_id': uc.role_id, 'id': uc.customer.id}
+            end
           end
-          end
-          
           render json: {data: user,'companies': companies }
         else
-          head(:unauthorized)
+          #head(:unauthorized)
+          render json: {data: "Usuario Inacativo", code: 401}
         end
-      end
-
-      def destroy
-
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,18 @@
 class ApplicationController < ActionController::API
   #acts_as_token_authentication_handler_for User, fallback: :exception
   #  respond_to :json
-
-private
+  private
+  
   def has_valid_api_key?
     token =  request.headers['X-User-Token']
     email =  request.headers['X-User-Email']
-    @user = User.where(authentication_token: token, email: email).first
+    @user = User.where(authentication_token: token, email: email).where(active: true).first
     @user.present?
-      
   end
 
   def validate_api_key!
+          render json: {data: "Usuario Inacativo", code: 401} unless has_valid_api_key?
 
-    return head :forbidden unless has_valid_api_key?
-    
-    
   end
 
 end
