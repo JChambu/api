fb8175921d3e78845ecf95ba5b872f8e4decb95d
Sergio <asergiop21@gmail.com>
2019-12-06 08:57:04 -0300

    Se quito el signo igual en consultas de datos de proyectos #11

--- app/models/project_data_child.rb
+++ app/models/project_data_child.rb
@@ -2,7 +2,7 @@ class ProjectDataChild < ApplicationRecord
   belongs_to :project
 
   def self.show_data_new project_type_id, updated_sequence, page
-    value = Project.joins(:project_data_child).where(project_type_id: project_type_id).where('project_data_children.update_sequence >= ?', updated_sequence).select("project_data_children.id, project_data_children.properties, project_data_children.updated_at,  project_data_children.user_id, project_data_children.project_id as project_data_id, project_data_children.project_field_id as project_field_id, project_data_children.update_sequence ").order('project_data_children.update_sequence').page(page).per_page(50)
+    value = Project.joins(:project_data_child).where(project_type_id: project_type_id).where('project_data_children.update_sequence > ?', updated_sequence).select("project_data_children.id, project_data_children.properties, project_data_children.updated_at,  project_data_children.user_id, project_data_children.project_id as project_data_id, project_data_children.project_field_id as project_field_id, project_data_children.update_sequence ").order('project_data_children.update_sequence').page(page).per_page(50)
     data = []
     geom_text = ''
     value.each do |row|
