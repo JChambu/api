15e1716dda19d1a0fc2dc8d1a7877c901bd72842
Sergio <asergiop21@gmail.com>
2019-12-13 09:57:30 -0300

    Se especifico mensajes de error al loguearse

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,24 +3,27 @@ module Api
     class SessionsController < ApplicationController
 
       def create
-        user_mail = User.where(email: params[:email]).where(active: true).first
+        user_mail = User.where(email: params[:email]).first
+        if !user_mail.nil? && user_mail&.valid_password?(params[:password])
+          if user_mail.active == true
+            user = {'email': user_mail.email, 'authentication_token': user_mail.authentication_token, 'user_id': user_mail.id}
+            companies=[]
+            user_customer = UserCustomer.where(user_id: user_mail)
 
-        if !user_mail.nil? && user_mail&.valid_password?(params[:password]) 
-          user = {'email': user_mail.email, 'authentication_token': user_mail.authentication_token, 'user_id': user_mail.id}
-          companies=[]
-          user_customer = UserCustomer.where(user_id: user_mail)
-
-          user_customer.map do |uc|
-            if uc.customer.subdomain == 'demo' 
-              companies << {'url': uc.customer.url, 'name_company': 'demo1', 'role_id': uc.role_id, 'id': uc.customer.id}
-            else
-              companies << {'url': uc.customer.url, 'name_company': uc.customer.subdomain, 'role_id': uc.role_id, 'id': uc.customer.id}
+            user_customer.map do |uc|
+              if uc.customer.subdomain == 'demo' 
+                companies << {'url': uc.customer.url, 'name_company': 'demo1', 'role_id': uc.role_id, 'id': uc.customer.id}
+              else
+                companies << {'url': uc.customer.url, 'name_company': uc.customer.subdomain, 'role_id': uc.role_id, 'id': uc.customer.id}
+              end
             end
+            render json: {data: user,'companies': companies }
+          else
+            render json: {data: "Usuario Inactivo", code: 401}
           end
-          render json: {data: user,'companies': companies }
         else
           #head(:unauthorized)
-          render json: {data: "Usuario Inacativo", code: 401}
+          render json: {data: "Email o Password Incorrecto", code: 401}
         end
       end
     end
