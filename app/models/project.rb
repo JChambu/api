fed9582bf84ce183fbe04012e61995d438bf5583
Sergio <asergiop21@gmail.com>
2019-12-20 19:03:00 -0300

    Limitar sincronizacion de los datos del usuario id 74

--- app/models/project.rb
+++ app/models/project.rb
@@ -32,9 +32,9 @@ class Project < ApplicationRecord
   def self.show_data_new project_type_id, updated_sequence, page, row_active
     type_geometry = ProjectType.where(id: project_type_id).pluck(:type_geometry)
     if (type_geometry[0] == 'Polygon')
-      value = Project.row_active(row_active).where(project_type_id: project_type_id).where('update_sequence > ?', updated_sequence).select("st_asgeojson(the_geom) as geom, id, properties, updated_at, project_status_id, user_id, the_geom, update_sequence, row_active").order(:update_sequence).page(page).per_page(50)
+      value = Project.row_active(row_active).where(project_type_id: project_type_id).where('update_sequence > ?', updated_sequence).where.not(user_id: '74').select("st_asgeojson(the_geom) as geom, id, properties, updated_at, project_status_id, user_id, the_geom, update_sequence, row_active").order(:update_sequence).page(page).per_page(50)
     else
-      value = Project.row_active(row_active).where(project_type_id: project_type_id).where('update_sequence > ?', updated_sequence).select("st_x(the_geom) as lng, st_y(the_geom) as lat, id, properties, updated_at, project_status_id, user_id, the_geom, update_sequence, row_active").order(:update_sequence).page(page).per_page(50)
+      value = Project.row_active(row_active).where(project_type_id: project_type_id).where('update_sequence > ?', updated_sequence).where.not(user_id: '74')select("st_x(the_geom) as lng, st_y(the_geom) as lat, id, properties, updated_at, project_status_id, user_id, the_geom, update_sequence, row_active").order(:update_sequence).page(page).per_page(50)
     end
     data = []
     geom_text = ''
--- app/models/project_data_child.rb
+++ app/models/project_data_child.rb
@@ -16,6 +16,7 @@ class ProjectDataChild < ApplicationRecord
         where(project_type_id: project_type_id).
         where('project_data_children.update_sequence > ?', updated_sequence).
         where('project_data_children.row_active = ?', row_active).
+        where('user_id = 74' ).
         select("project_data_children.id, project_data_children.properties, project_data_children.updated_at,  project_data_children.user_id, project_data_children.project_id as project_data_id, project_data_children.project_field_id as project_field_id, project_data_children.update_sequence, project_data_children.row_active ").order('project_data_children.update_sequence').page(page).per_page(50)
     data = []
     geom_text = ''
