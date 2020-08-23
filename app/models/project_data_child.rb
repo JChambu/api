e8d0e9048ddfd7a9ad094c3c72ada45466f98be4
jvillegas88 <villegas.jonathan88@gmail.com>
2020-08-23 15:32:16 -0300

    Aplica filtro por atributos al endpoint list_data_children #53

--- app/models/project_data_child.rb
+++ app/models/project_data_child.rb
@@ -36,6 +36,11 @@ class ProjectDataChild < ApplicationRecord
     @owner = ProjectFilter.where(user_id: current_user).where(project_type_id: project_type_id).pluck(:owner).first
     value = value.where(user_id: current_user) if !@owner.nil? && @owner != false
 
+    # Aplica filtro por atributo
+    @project_filters = ProjectFilter.where(user_id: current_user).where(project_type_id: project_type_id).first
+    @project_filters.properties.to_a.each do |prop|
+      value = value.where(" projects.properties->>'" + prop[0] + "' = '#{prop[1]}'")
+    end
     value = value.order('project_data_children.update_sequence').page(page).per_page(50)
     data = []
     geom_text = ''
