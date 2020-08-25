31d70b63b4d534a2ec750d6de77a9e9800ce95aa
jvillegas88 <villegas.jonathan88@gmail.com>
2020-08-24 21:41:30 -0300

    Ordena código de project_field para mejor legibilidad

--- app/models/project_field.rb
+++ app/models/project_field.rb
@@ -7,15 +7,20 @@ class ProjectField < ApplicationRecord
   def self.show_schema_new data
 
     project = []
-    project_field = ProjectField.where(project_type_id: data.id).select(:id, :name, :field_type_id , :required, :choice_list_id, :regexp_type_id, :hidden, :read_only, :sort, :popup, :calculated_field, :role, :data_script, :filter_field ).order(:sort)
+    project_field = ProjectField.where(project_type_id: data.id).select(
+      :id, :name, :field_type_id, :required, :choice_list_id, :regexp_type_id, :hidden,
+      :read_only, :sort, :popup, :calculated_field, :role, :data_script, :filter_field
+    ).order(:sort)
+
     project_field.each do |row|
-
+      
       @rr = row
-      @choice_list_item = ''
 
+      @choice_list_item = ''
       if !row.choice_list_id.nil?
         @choice_list_item = show_choice_list(row.choice_list_id)
       end
+
       @regexp =''
       if !row.regexp_type_id.nil?
         @regexp = show_regexp_type(row.regexp_type_id)
@@ -29,39 +34,78 @@ class ProjectField < ApplicationRecord
       @role = row.role
       @data_script = row.data_script
       @filter_field = row.filter_field
-        @subvalue = []
+      @subvalue = []
+
       if row.field_type_id == 7
-        @repetible = ProjectSubfield.where(project_field_id: row.id).select(:id, :name, :field_type_id , :required, :choice_list_id, :regexp_type_id, :hidden, :read_only, :popup, :calculated_field, :role, :data_script ).order(:sort)
+
+        @repetible = ProjectSubfield.where(project_field_id: row.id).select(
+          :id, :name, :field_type_id , :required, :choice_list_id, :regexp_type_id, :hidden,
+          :read_only, :popup, :calculated_field, :role, :data_script
+        ).order(:sort)
+
         @repetible.each do |sub_row|
-                   @choice_list_subitem = ''
-                   if !sub_row.choice_list_id.nil?
-                     @choice_list_subitem = show_choice_list(sub_row.choice_list_id)
-                   end
-                   @regexp_subitem =''
-                   if !sub_row.regexp_type_id.nil?
-                     @regexp_subitem = show_regexp_type(sub_row.regexp_type_id)
-                   end
-                    if !@repetible.empty?
-                      @subvalue.push(sub_row.as_json.merge("name":sub_row.name, "items":  @choice_list_subitem , "regexp": @regexp_subitem, "field_type_id": sub_row.field_type_id, "required": sub_row.required, "hidden":sub_row.hidden, "read_only":sub_row.read_only, "popup":sub_row.popup, "calculated":sub_row.calculated_field, "role":sub_row.role, "data_script": sub_row.data_script))
-                    end
+
+          @choice_list_subitem = ''
+          if !sub_row.choice_list_id.nil?
+            @choice_list_subitem = show_choice_list(sub_row.choice_list_id)
+          end
+
+          @regexp_subitem =''
+          if !sub_row.regexp_type_id.nil?
+            @regexp_subitem = show_regexp_type(sub_row.regexp_type_id)
+          end
+
+          if !@repetible.empty?
+            @subvalue.push(sub_row.as_json.merge(
+              "name":sub_row.name,
+              "items":  @choice_list_subitem,
+              "regexp": @regexp_subitem,
+              "field_type_id": sub_row.field_type_id,
+              "required": sub_row.required,
+              "hidden":sub_row.hidden,
+              "read_only":sub_row.read_only,
+              "popup":sub_row.popup,
+              "calculated":sub_row.calculated_field,
+              "role":sub_row.role,
+              "data_script": sub_row.data_script
+            ))
+          end
+
         end
       end
 
+      @pf = {
+        "id":row.id,
+        "name": row.name,
+        "field_type_id":row.field_type_id,
+        "items": @choice_list_item,
+        "required": row.required,
+        "regexp": @regexp,
+        "hidden": @hidden,
+        "sort": @sort,
+        "elements":@subvalue,
+        "read_only":@readonly,
+        "popup":@popup,
+        "calculated": @calculated,
+        "role":@role,
+        "data_script": @data_script,
+        filter_field: @filter_field
+      }
 
-      @pf = { "id":row.id, "name": row.name, "field_type_id":row.field_type_id, "items": @choice_list_item, "required": row.required, "regexp": @regexp, "hidden": @hidden, "sort": @sort, "elements":@subvalue, "read_only":@readonly, "popup":@popup, "calculated": @calculated, "role":@role, "data_script": @data_script, filter_field: @filter_field }
       project.push @pf
       @pp = project
     end
-    project
+    return project
   end
+
 
   def self.status_types project_type_id
     @project_statuses = ProjectStatus.where(project_type_id: project_type_id).select(:id, :name, :color).order(:name)
   end
 
 
-
   def self.show_choice_list id
+
     items=[]
     choice_list = ChoiceList.find(id)
     choice_list_item  = ChoiceListItem.where(choice_list_id: choice_list.id)
@@ -69,7 +113,8 @@ class ProjectField < ApplicationRecord
     sorted_choice_list_items.each do |i|
       items << {"id": i.id, "name":i.name}
     end
-    items
+    return items
+
   end
 
 
@@ -78,4 +123,5 @@ class ProjectField < ApplicationRecord
     regexp = r.expresion
     regexp
   end
+
 end
