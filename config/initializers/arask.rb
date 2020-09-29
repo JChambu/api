018802eab97d02ac851d130f6971f45ac95264c8
jvillegas88 <villegas.jonathan88@gmail.com>
2020-09-29 00:24:23 -0300

    Actualiza cron arask

--- config/initializers/arask.rb
+++ config/initializers/arask.rb
@@ -28,6 +28,6 @@ Arask.setup do |arask|
   #end
 
   # Resetea los estados todos los días a las 00hs
-  arask.create task: 'statuses:reset', cron: '00 00 * * * '
+  arask.create task: 'statuses:reset', cron: '0 0 * * * '
 
 end
