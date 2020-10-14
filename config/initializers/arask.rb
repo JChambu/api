496058a50e74a265cdb462f6842ab4c6871d7a13
jvillegas88 <villegas.jonathan88@gmail.com>
2020-10-13 21:32:46 -0300

    Agrega run_first_time: true

--- config/initializers/arask.rb
+++ config/initializers/arask.rb
@@ -29,6 +29,6 @@ Arask.setup do |arask|
   #end
 
   # Resetea los estados todos los días a las 00hs
-  arask.create task: 'statuses:reset', cron: '0 0 * * *'
+  arask.create task: 'statuses:reset', cron: '0 0 * * *', run_first_time: true
 
 end
