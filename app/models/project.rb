63364aea0808c1597e4cb404349364e7b78e4226
jvillegas88 <villegas.jonathan88@gmail.com>
2020-11-05 13:40:49 -0300

    Aplica distinct en padres para evitar duplicados #82

--- app/models/project.rb
+++ app/models/project.rb
@@ -41,7 +41,7 @@ class Project < ApplicationRecord
   def self.row_quantity project_type_id, updated_sequence, row_active, current_season, current_user
 
     @rows = Project
-      .select('main.*')
+      .select('DISTINCT main.*')
       .from('projects main')
       .check_row_active(row_active)
       .check_current_season(current_season)
@@ -191,7 +191,7 @@ class Project < ApplicationRecord
 
     end
 
-    # TODO: Pensar si la utilización del update_sequence sólo en el main puede traer alguna complicacion al sync
+    value = value.distinct
     value = value.order('main.update_sequence').page(page).per_page(50)
     data = []
     geom_text = ''
