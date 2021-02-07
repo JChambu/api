2a8f04ecc5aeea98a07033effeb3bd73f5b5563f
Jonathan Villegas <villegas.jonathan88@gmail.com>
2021-02-07 17:59:06 -0300

    Fix/Corrige array hijos (#109)
    * Corrige búsqueda en array hijo
    
    * Elimina array al enviar hijos

--- app/models/project_data_child.rb
+++ app/models/project_data_child.rb
@@ -90,14 +90,9 @@ class ProjectDataChild < ApplicationRecord
     value.each do |row|
 
       form = {}
-      row.properties[0].each do |k, v|
+      row.properties.each do |k, v|
         form.merge!("#{k}": v)
       end
-
-      # REVIEW: Envuelve el hash en un array para compatibilidad con el formato,
-      # el array no debería existir y se debería enviar sólo el hash
-      form_array = []
-      form_array.push(form)
 
       # Si esta eliminado, pertenece a la temporada anterior o está desabilitado envía row_active como false para eliminar en GWMobile
       if row.row_active == false || row.current_season == false || row.row_enabled == false
@@ -112,7 +107,7 @@ class ProjectDataChild < ApplicationRecord
         "id": row.id,
         "project_data_id": row.project_data_id,
         "project_field_id": row.project_field_id,
-        "properties": form_array,
+        "properties": form,
         "gwm_created_at": row.gwm_created_at,
         "gwm_updated_at": row.gwm_updated_at,
         "user_id": row.user_id,
